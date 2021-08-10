17ff611d3ad17d8ad3448f1a289233e48e07f91c
ALTIMETRIK\lkupferberg <lkupferberg@altimetrik.com>
2021-08-10 16:54:03 -0400

    -- Added the project. -- Added the dockerfile to use docker. -- Added automatic route mapping.

---
+++ gatewayserver/src/test/java/com/optimagrowth/gatewayserver/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.optimagrowth.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
