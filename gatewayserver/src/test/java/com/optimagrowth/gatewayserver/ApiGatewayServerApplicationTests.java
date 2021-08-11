70ed84f6d7a2feb0ac273970d39d3f20f034b4f4
ALTIMETRIK\lkupferberg <lkupferberg@altimetrik.com>
2021-08-11 15:55:59 -0400

    -- Removed tests code.

--- gatewayserver/src/test/java/com/optimagrowth/gatewayserver/ApiGatewayServerApplicationTests.java
+++ gatewayserver/src/test/java/com/optimagrowth/gatewayserver/ApiGatewayServerApplicationTests.java
@@ -1,11 +1,8 @@
 package com.optimagrowth.gatewayserver;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiGatewayServerApplicationTests {
-
-  @Test
-  void contextLoads() {}
-}
+// @SpringBootTest
+// class ApiGatewayServerApplicationTests {
+//
+//  @Test
+//  void contextLoads() {}
+// }
