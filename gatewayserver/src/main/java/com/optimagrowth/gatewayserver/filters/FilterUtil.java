c3132cb014de27a0c0000590b55510ba529d9f64
ALTIMETRIK\lkupferberg <lkupferberg@altimetrik.com>
2021-08-18 22:59:19 -0400

    -- Added the custom JWT parameter decoding. -- Fixed the auth param name.

--- gatewayserver/src/main/java/com/optimagrowth/gatewayserver/filters/FilterUtil.java
+++ gatewayserver/src/main/java/com/optimagrowth/gatewayserver/filters/FilterUtil.java
@@ -10,10 +10,20 @@ import org.springframework.web.server.ServerWebExchange;
 public class FilterUtil {
 
   public static final String CORRELATION_ID = "tmx-correlation-id";
+  public static final String AUTH_TOKEN = "Authorization";
 
   public String getCorrelationId(HttpHeaders requestHeaders) {
     if (!Objects.isNull(requestHeaders.get(CORRELATION_ID))) {
       List<String> header = requestHeaders.get(CORRELATION_ID);
+      return header.stream().findFirst().get();
+    } else {
+      return null;
+    }
+  }
+
+  public String getAuthToken(HttpHeaders requestHeaders) {
+    if (!Objects.isNull(requestHeaders.get(AUTH_TOKEN))) {
+      List<String> header = requestHeaders.get(AUTH_TOKEN);
       return header.stream().findFirst().get();
     } else {
       return null;
--- gatewayserver/src/main/java/com/optimagrowth/gatewayserver/filters/TrackingFilter.java
+++ gatewayserver/src/main/java/com/optimagrowth/gatewayserver/filters/TrackingFilter.java
@@ -3,6 +3,8 @@ package com.optimagrowth.gatewayserver.filters;
 import java.util.Objects;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+import org.json.JSONObject;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
@@ -35,6 +37,7 @@ public class TrackingFilter implements GlobalFilter {
       exchange = filterUtil.setCorrelationId(exchange, correlationId);
       log.debug("{} generated in tracking filter: {}", FilterUtil.CORRELATION_ID, correlationId);
     }
+    log.debug("The authentication name from the token is : {}", getAuthenticationName(header));
     return chain.filter(exchange);
   }
 
@@ -44,5 +47,25 @@ public class TrackingFilter implements GlobalFilter {
 
   private String generateCorrelationId() {
     return UUID.randomUUID().toString();
+  }
+
+  private String getAuthenticationName(HttpHeaders requestHeaders) {
+    String authenticationName = "";
+    String authToken = filterUtil.getAuthToken(requestHeaders);
+    if (!Objects.isNull(authToken)) {
+      authToken = authToken.replace("Bearer ", "");
+      JSONObject jsonObject = decodeJWT(authToken);
+      authenticationName = jsonObject.getString("authentication_name");
+    }
+    return authenticationName;
+  }
+
+  private JSONObject decodeJWT(String JWTToken) {
+    String[] splitString = JWTToken.split("\\.");
+    String base64EncodedBody = splitString[1];
+    Base64 base64Url = new Base64(true);
+    String body = new String(base64Url.decode(base64EncodedBody));
+
+    return new JSONObject(body);
   }
 }
