0cdb9e148b98df388fcb93da1f189dff72702450
ALTIMETRIK\lkupferberg <lkupferberg@altimetrik.com>
2021-08-18 23:21:43 -0400

    -- Added the JWT config,

---
+++ organization-service/src/main/java/com/optimagrowth/organization/config/ServiceConfig.java
@@ -0,0 +1,15 @@
+package com.optimagrowth.organization.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+public class ServiceConfig {
+
+  @Value("${signing.key}")
+  @Getter
+  private String jwtSigningKey = "";
+}
---
+++ organization-service/src/main/java/com/optimagrowth/organization/security/JWTTokenEnhancer.java
@@ -0,0 +1,23 @@
+package com.optimagrowth.organization.security;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+public class JWTTokenEnhancer implements TokenEnhancer {
+
+  @Override
+  public OAuth2AccessToken enhance(
+      OAuth2AccessToken oAuth2AccessToken, OAuth2Authentication oAuth2Authentication) {
+    Map<String, Object> additionalInfo = new HashMap<>();
+
+    additionalInfo.put("authentication_name", oAuth2Authentication.getName());
+
+    ((DefaultOAuth2AccessToken) oAuth2AccessToken).setAdditionalInformation(additionalInfo);
+
+    return oAuth2AccessToken;
+  }
+}
